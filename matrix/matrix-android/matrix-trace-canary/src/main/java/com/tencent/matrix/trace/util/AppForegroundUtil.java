bf129003f3e4339d49df16628d5416d80fce495c
jiayuliang <jiayuliang@kanzhun.com>
2021-10-26 16:30:17 +0800

    注释

--- matrix/matrix-android/matrix-trace-canary/src/main/java/com/tencent/matrix/trace/util/AppForegroundUtil.java
+++ matrix/matrix-android/matrix-trace-canary/src/main/java/com/tencent/matrix/trace/util/AppForegroundUtil.java
@@ -31,7 +31,7 @@ import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
-
+//AppForegroundUtil这个类主要isActivityInterestingToUser方法判断用户是否在前台
 public enum AppForegroundUtil {
 
     INSTANCE;
