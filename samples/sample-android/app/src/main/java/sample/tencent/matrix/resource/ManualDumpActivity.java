ad2ed0d75e035db871198e2ee0271f4fbb448497
jiayuliang <jiayuliang@kanzhun.com>
2022-01-26 16:18:09 +0800

    2.0.5-SNAPSHOT

--- samples/sample-android/app/src/main/java/sample/tencent/matrix/resource/ManualDumpActivity.java
+++ samples/sample-android/app/src/main/java/sample/tencent/matrix/resource/ManualDumpActivity.java
@@ -23,19 +23,19 @@ public class ManualDumpActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual_dump);
 
-        ((TextView) findViewById(R.id.leak_activity))
-                .setText(getIntent().getStringExtra(SharePluginInfo.ISSUE_ACTIVITY_NAME));
-        ((TextView) findViewById(R.id.leak_process))
-                .setText(getIntent().getStringExtra(SharePluginInfo.ISSUE_LEAK_PROCESS));
-
-        final ManualDumpProcessor.ManualDumpData data =
-                getIntent().getParcelableExtra(SharePluginInfo.ISSUE_DUMP_DATA);
-        if (data != null) {
-            ((TextView) findViewById(R.id.reference_chain))
-                    .setText(data.refChain);
-        } else {
-            ((TextView) findViewById(R.id.reference_chain))
-                    .setText(R.string.empty_reference_chain);
-        }
+//        ((TextView) findViewById(R.id.leak_activity))
+//                .setText(getIntent().getStringExtra(SharePluginInfo.ISSUE_ACTIVITY_NAME));
+//        ((TextView) findViewById(R.id.leak_process))
+//                .setText(getIntent().getStringExtra(SharePluginInfo.ISSUE_LEAK_PROCESS));
+//
+//        final ManualDumpProcessor.ManualDumpData data =
+//                getIntent().getParcelableExtra(SharePluginInfo.ISSUE_DUMP_DATA);
+//        if (data != null) {
+//            ((TextView) findViewById(R.id.reference_chain))
+//                    .setText(data.refChain);
+//        } else {
+//            ((TextView) findViewById(R.id.reference_chain))
+//                    .setText(R.string.empty_reference_chain);
+//        }
     }
 }
